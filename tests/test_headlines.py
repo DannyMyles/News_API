63d718c9131cb4cd80fe024c768b05585eae2390
DannyMyles <dancun.wawire@student.moringaschool.com>
2021-11-01 13:45:14 +0300

    deployment to heroku

--- tests/test_headlines.py
+++ tests/test_headlines.py
@@ -19,3 +19,4 @@ class HeadlinesTest(unittest.TestCase):
         self.assertTrue(isinstance(self.new_headlines,Headlines))
 
 # isinstance:::: Return true if the object argument is an instance of the classinfo argument, or of a (direct, indirect or virtual) subclass thereof. If object is not an object of the given type, the function always returns false.
+
